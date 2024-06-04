ad499ce229a1b9f05f76f0711d03324601ecd8b9
romashkarrr <r0man.kish999@gmail.com>
2024-06-04 15:34:33 +0300

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by romashka on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
